d66005c458ca626d9b311ac3eb2262e415915b1d
Jon Palmisciano <jp@jonpalmisc.com>
2020-08-28 22:13:33 -0400

    docs: set up Doxygen; wrote function docs

--- mpatch.h
+++ mpatch.h
@@ -1,3 +1,5 @@
+/// @file
+
 #ifndef LIBMPATCH_MPATCH_H
 #define LIBMPATCH_MPATCH_H
 
@@ -6,22 +8,73 @@
 
 #include <mach/error.h>
 
+// I'm generally against doing this but The compiler barks about
+// mp_get_proc_list's definition not matching between the header and source
+// file without this for some reason.
 typedef struct kinfo_proc kinfo_proc;
 
-// Word-aligns a given size/length.
+/**
+ * Word-aligns a given size.
+ *
+ * @param size the size to word-align
+ * @return the word-aligned size
+ */
 size_t mp_word_align(size_t size);
 
-// Reads a given process' memory.
+/**
+ * Reads \p len bytes from the memory of the process with the ID of \p pid,
+ * starting at \p addr.
+ *
+ * Be aware that \p addr is absolute, so if you are attempting to read data at
+ * a specific file offset, you will first need to add the offset to the base
+ * address of the process due to ASLR. See mp_get_proc_base_addr().
+ *
+ * @param pid the ID of the target process
+ * @param addr the address to start reading from
+ * @param len the number of bytes to read
+ */
 unsigned char *mp_read(int pid, void *addr, size_t len);
 
-// Write to a given process' memory.
+/**
+ * Writes \p len bytes from the buffer \p data to the memory of the process
+ * with the ID of \p pid, starting at \p addr.
+ *
+ * @param pid the ID of the process to read from
+ * @param addr the address to start reading at
+ * @param data the buffer of bytes to write
+ * @param len the number of bytes to write (should be the length of \p data)
+ * @return the Mach error code indicating the success/failure of the operation
+ */
 kern_return_t mp_write(int pid, void *addr, unsigned char *data, size_t len);
 
-// Finds a process by name.
+/**
+ * Gets the process ID of the process with the given name.
+ *
+ * @param procname the name of the process to find
+ */
 int32_t mp_get_pid(char *procname);
+
+/**
+ * Gets a list of all running processes.
+ *
+ * @param [out] list the list of running processes
+ * @param [out] count the length of \p list (number of processes)
+ * @return the Mach error code indicating the success/failure of the operation
+ */
 static int mp_get_proc_list(kinfo_proc **list, size_t *count);
 
+/**
+ * Gets the base address (post-ASLR) of the process with the ID of \p pid.
+ *
+ * This function will be necessary if attempting to patch memory based off of
+ * known file offsets. First get the base address, then add the offset to the
+ * return value of this function.
+ *
+ * @param pid the ID of the process to read from
+ * @return the base address of the process
+ */
 uint64_t mp_get_proc_base_addr(int pid);
+
 int mp_set_page_exec(void *address);
 
 #endif
